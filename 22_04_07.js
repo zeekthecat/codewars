ef1b6af83efc148fc8bfbd3ccdf8ee788c717ee5
Jennifer Akeley <88170570+zeekthecat@users.noreply.github.com>
2022-04-07 12:47:53 -0400

    add 4/7 challenge

---
+++ 22_04_07.js
@@ -0,0 +1,16 @@
+// You have to write a function that accepts three parameters:
+// cap is the amount of people the bus can hold excluding the driver.
+// on is the number of people on the bus excluding the driver.
+// wait is the number of people waiting to get on to the bus excluding the driver.
+// If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
+// 8 kyu
+
+function enough(cap, on, wait) {
+    if (wait <= (cap- on)) {
+      return 0
+    }
+    else if (wait > cap - on) {
+      return wait - (cap - on)
+    }
+  }
