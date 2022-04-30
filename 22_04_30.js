8c398a7ebfb352866d5921929f9cbafcd8c253a3
Jennifer Akeley <88170570+zeekthecat@users.noreply.github.com>
2022-04-30 15:22:05 -0400

    add 4/30 challenge

---
+++ 22_04_30.js
@@ -0,0 +1,10 @@
+/*
+Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string).
+
+8 kyu
+*/
+
+function sumStr(a,b) {
+    let sum = Number(a) + Number(b)
+    return sum.toString()
+  }
