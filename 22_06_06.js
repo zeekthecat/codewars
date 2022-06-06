f5aaadc59a6d14fba0c489b187fa71a0b0d1210d
Jenni Akeley <jwyliephoto@gmail.com>
2022-06-06 10:55:51 -0400

    add 6/6 challenge

---
+++ 22_06_06.js
@@ -0,0 +1,11 @@
+/*
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+
+8kyu
+*/
+
+function fakeBin(x){
+    return x.replace(/[0-4]/g, '0').replace(/[5-9]/g, '1')
+}
