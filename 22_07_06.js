0063b670bac580b0fbbb6659aa285c94b152dbe2
Jenni Akeley <jwyliephoto@gmail.com>
2022-07-06 09:20:41 -0400

    add 7/6 challenge

---
+++ 22_07_06.js
@@ -0,0 +1,20 @@
+/*
+Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+8kyu
+*/
+
+function getGrade (s1,s2,s3){
+    let average = (s1 + s2 + s3) / 3
+    if (average >= 90){
+      return 'A'
+    } else if (average >= 80){
+      return 'B'
+    } else if (average >= 70){
+      return 'C'
+    } else if (average >= 60){
+      return 'D'
+    } else {
+      return 'F'
+    }
+}
