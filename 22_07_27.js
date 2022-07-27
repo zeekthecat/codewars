acef65e40a2bc2191ecb9d239f0f35f83aaba769
Jenni Akeley <jwyliephoto@gmail.com>
2022-07-27 08:20:00 -0400

    add 7/27 challenge

---
+++ 22_07_27.js
@@ -0,0 +1,9 @@
+/*
+Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+8kyu
+*/
+
+function getChar(c){
+    return String.fromCharCode(c)
+}
