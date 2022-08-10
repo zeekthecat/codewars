26b04576b8ffc3ed5c7682d5069294762939106a
Jenni Akeley <jwyliephoto@gmail.com>
2022-08-10 13:00:33 -0400

    add 8/10 challenge

---
+++ 22_08_10.js
@@ -0,0 +1,7 @@
+/*
+This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+8kyu
+*/
+
+const solve = (x,y) => x / y
