7f8d261866148c4a3f7207031e1d3b132cf1aa34
Jenni Akeley <jwyliephoto@gmail.com>
2022-08-28 13:37:50 -0400

    add 8/28 challenge

---
+++ 22_08_28.js
@@ -0,0 +1,13 @@
+/*
+You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
+
+8kyu
+*/
+
+let items = []
+const itemsToPush = {
+  a: 'b',
+  c: 'd'
+}
+
+items.push(itemsToPush)
