944b0f1c82bbebb3b930be045b8f945e5182a56a
Jenni Akeley <jwyliephoto@gmail.com>
2022-09-13 19:09:21 -0400

    add 9/13 challenge

---
+++ 22_09_13.js
@@ -0,0 +1,9 @@
+/*
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+
+7kyu
+*/
+
+const addBinary = (a,b) => (a + b).toString(2)
