427f6987780c96a72cdbe94b3ceda29560aa4f71
Jenni Akeley <jwyliephoto@gmail.com>
2022-11-15 08:24:15 -0500

    add 11/15 challenge

---
+++ 22_11_15.js
@@ -0,0 +1,15 @@
+/*
+Coding in function bigToSmall. function accept 1 parameter arr(2D number array).
+
+Your task is: First to all, refer to the example above, flat output arr to a one-dimensional array.
+
+And then sort array in descending order.
+
+Finally, use the separator ">" to connect the elements into a string.
+
+Don't complain about the situation like 1>1 is not reasonable, it is just a separator.
+
+8kyu
+*/
+
+const bigToSmall = arr => [].concat(...arr).sort((a,b) => b - a).join('>')
