dafa5251747901e312437964d092ada6aaa5a9b3
Jenni Akeley <jwyliephoto@gmail.com>
2022-11-30 11:22:42 -0500

    add 11/30 challenge

---
+++ 22_11_30.js
@@ -0,0 +1,11 @@
+/*
+In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+
+years divisible by 4 are leap years
+but years divisible by 100 are not leap years
+but years divisible by 400 are leap years
+
+7kyu
+*/
+
+const isLeapYear = year => year % 100 !== 0 && year % 4 == 0 || year % 400 == 0
